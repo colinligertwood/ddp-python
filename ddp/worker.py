3601211dec82f837596d22b54fe563e05f177210
Colin Ligertwood <colin@zaber.com>
2015-09-22 16:59:18 -0700

    more code reorg

--- ddp/worker.py
+++ ddp/worker.py
@@ -13,12 +13,17 @@ class Worker(threading.Thread):
         global ddp_subscriptions
 
         while self.active:
-            message = self.queue.get()
+            message = ddp_message_queue.get()
             if message.msg in ['added', 'changed', 'removed']: 
                 rec = (message.collection, message.id)
-                for sub in self.subscriptions:
+                for sub in ddp_subscriptions:
                     if not sub.has_rec(rec):
                         continue
                     sub.conn.write_message(message)
 
+            if message.msg in ['ready']:
+                for sub in ddp_subscriptions:
+                    if not sub.id == message.id:
+                        continue
+                    sub.conn.write_message(message)
 
