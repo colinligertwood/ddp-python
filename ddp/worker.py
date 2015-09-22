5fbc6b0e58671efc270de1ef97c656f96d40e227
Colin Ligertwood <colin@zaber.com>
2015-09-22 16:29:51 -0700

    add worker class in its own file

---
+++ ddp/worker.py
@@ -0,0 +1,22 @@
+class Worker(threading.Thread):
+    """
+    """
+
+    def __init__(self, queue, subscriptions):
+        self.queue = queue
+        self.subscriptions = subscriptions
+
+    def start(self):
+        while self.active:
+            message = self.queue.get()
+            if message.msg not in ['added', 'changed', 'removed', 'ready', 'result']:
+                continue
+           
+            if message.msg in ['added', 'changed', 'removed']: 
+                rec = (message.collection, message.id)
+                for sub in self.subscriptions:
+                    if not sub.has_rec(rec):
+                        continue
+                    sub.conn.write_message(message)
+
+
