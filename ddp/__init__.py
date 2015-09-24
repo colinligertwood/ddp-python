8806b05ff714e7ddd71bf0a1b92f2ad089782a73
Colin Ligertwood <colin@zaber.com>
2015-09-24 16:13:42 -0700

    move subscription into its own class file

--- ddp/__init__.py
+++ ddp/__init__.py
@@ -4,4 +4,5 @@ from client import Client
 from server import Server
 from handler import Handler
 from worker import Worker
+from subscription import Subscription
 
--- ddp/ddp.py
+++ ddp/ddp.py
@@ -8,37 +8,6 @@ class MessageQueue(Queue.Queue):
     """
     """
 
-class Subscription(object):
-    """
-    """
-
-    def __init__(self, id, name, params, conn):
-        self.id = id
-        self.active = True
-        self.recs = []
-        self.conn = conn 
-        self.name = name
-        self.params = params
-
-    def has_rec(self, collection, id):
-        return (collection,id) in self.recs
-
-    def add_rec(self, collection, id):
-        self.recs.append((collection,id))
-
-    def remove_rec(self, collection, id):
-        self.recs.remove((collection,id))
-
-    def reset(self):
-        self.recs = []
-
-    def start(self):
-        self.active = True
-
-    def stop(self):
-        self.active = False
-
-
 class Message(object):
     """
     """
