80147b8d874962a2d3538090b045c8e396e7bc11
Colin Ligertwood <colin@zaber.com>
2015-10-13 14:02:56 -0700

    add reaper

---
+++ ddp/reaper.py
@@ -0,0 +1,28 @@
+from time import sleep
+from globals import *
+
+class Reaper(object):
+    def __init__(self):
+        """
+        """
+    def start(self):
+        global ddp_sessions
+        global ddp_subscriptions
+        while True:
+            sleep(10)
+            reap_sessions = []
+            reap_subscriptions = []
+            for ddp_session_id, session in ddp_sessions.items():
+                if session.has_expired():
+                    reap_sessions.append(ddp_session_id)
+
+            for subscription in ddp_subscriptions:
+                if subscription.method == 'reapme':
+                    reap_subscriptions.append(subscription.id)
+                    
+            for ddp_session_id in reap_sessions:
+                ddp_subscriptions.remove_session(ddp_session_id)
+                del ddp_sessions[ddp_session_id]
+
+            for ddp_subscription_id in reap_subscriptions:
+                ddp_subscriptions.remove_id(ddp_subscription_id)
