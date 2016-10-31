31791f204d0d4f220cace2f1e9ab896682e48f61
Colin Ligertwood <colin@zaber.com>
2016-10-31 11:50:31 -0700

    allow tornado ioloop to quit on sigint

--- ddp/server.py
+++ ddp/server.py
@@ -3,6 +3,7 @@ import tornado.ioloop
 import tornado.web
 from sockjs.tornado import transports, SockJSRouter
 import socket
+import signal
 
 class Server(object):
     def __init__(self, handler, baseurl, port):
@@ -22,6 +23,7 @@ class Server(object):
         self.http_server = tornado.httpserver.HTTPServer(self.application)
         self.http_server.listen(self.port)
         self.ioloop = tornado.ioloop.IOLoop.instance()
+        signal.signal(signal.SIGINT, lambda sig, frame: self.ioloop.add_callback_from_signal(self.stop))
         self.ioloop.start()
         #HACK Workaround a bug in tornado that prevents server restarts
         self.ioloop._timeouts = []
